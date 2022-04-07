30c2927267a23de38fce8995ee9404c9d71cade7
Sebastian <hoppe@sebastianhoppe.com>
2022-04-07 12:05:04 +0200

    kubernetes

---
+++ kubernetes/main.tf
@@ -0,0 +1,25 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg_kubernetes" {
+  name     = "RG-${var.location_short}-CONTAINER-${random_integer.net_id.result}-${var.environment}"
+  location = "${var.location}"
+}
+
+resource "azurerm_kubernetes_cluster" "aks_test" {
+  name                = "AKS-${var.location_short}-${random_integer.net_id.result}-${var.environment}"
+  location            = "${var.location}"
+  resource_group_name = azurerm_resource_group.rg_kubernetes.name
+  dns_prefix          = "AKS-${var.location_short}-${random_integer.net_id.result}-${var.environment}"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2s_v3"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
